e215288dc515e3bcbc567a9392b4025da57a73a8
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-05-27 22:23:38 -0500

    UI updates to modal controller for state

--- ToDoHelper/ViewControllers/TDDropBoxViewController.m
+++ ToDoHelper/ViewControllers/TDDropBoxViewController.m
@@ -7,19 +7,76 @@
 //
 
 #import "TDDropBoxViewController.h"
+#import <dropbox/dropbox.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface TDDropBoxViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *linkWithDropboxButton;
+@property (weak, nonatomic) IBOutlet UIButton *unlinkButton;
+@property (weak, nonatomic) IBOutlet UIButton *saveToDropboxButton;
+@property (weak, nonatomic) IBOutlet UIButton *cancelButton;
 
 @end
 
 @implementation TDDropBoxViewController
 
 
+#pragma mark - life cycle
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+	
+    [self initializeButtonAppearance:self.linkWithDropboxButton];
+    [self initializeButtonAppearance:self.unlinkButton];
+    [self initializeButtonAppearance:self.saveToDropboxButton];
+    [self initializeButtonAppearance:self.cancelButton];
+    [self configureButtons];
+    
+    //  determine appropriate buttons to enable userinteraction, depending on
+    //  whether a dropbox account is linked or not
+    
+    
+    
+    
 }
 
+
+
+
+#pragma mark - helpers
+- (void)configureButtons
+{
+    //  detemine which buttons should be enabled, based on whether a dropbox account has
+    //  been linked or not
+    if ([DBAccountManager sharedManager].linkedAccount) {
+        
+        self.linkWithDropboxButton.userInteractionEnabled = NO;
+        self.linkWithDropboxButton.backgroundColor = [UIColor grayColor];
+    }
+    else {
+        
+        self.unlinkButton.userInteractionEnabled = NO;
+        self.unlinkButton.backgroundColor = [UIColor grayColor];
+        
+        self.saveToDropboxButton.userInteractionEnabled = NO;
+        self.saveToDropboxButton.backgroundColor = [UIColor grayColor];
+        
+    }
+}
+
+- (void)initializeButtonAppearance:(UIButton*)btn
+{
+    //  make a custom button look like a default button
+    [btn.layer setCornerRadius:8.0f];
+    [btn.layer setMasksToBounds:YES];
+    [btn.layer setBorderWidth:1.0f];
+    [btn.layer setBorderColor:[[UIColor blackColor] CGColor]];
+    
+    
+    //  set highlighted properties
+    [btn setTitleColor:[UIColor orangeColor] forState:UIControlStateHighlighted];
+    
+}
 
 @end
